2f0e099b3b7fac717cc1a458818b9fb2238d2e1d
tetnuldie <137920481+tetnuldie@users.noreply.github.com>
2023-06-29 19:34:39 +0200

    Add files via upload

---
+++ My_Project/src/first.java
@@ -0,0 +1,9 @@
+public class first {
+    public static void main(String[] args) {
+        int x = 2_147_483_647;
+        int s = -2_147_483_648;
+        x=x+1;
+        s=s-10;
+        System.out.println("x= "+x+"\ns= "+s);
+    }
+}
---
+++ My_Project/src/var_types.java
@@ -0,0 +1,22 @@
+public class var_types {
+    public static void main(String[] args) {
+/*        int x = 2_147_483_647;
+        x = x + 1;
+        if (x <= 0) {
+            System.out.println("ololo");
+        }else{
+            System.out.println("trololo");
+        }
+        System.out.println("x= " + x);
+ */
+        int a=43;
+        int b=7;
+        int mul;
+        float div;
+
+        mul = a*b;
+        div=(float)a/b;
+
+        System.out.println(mul+"\n"+div);
+    }
+}
